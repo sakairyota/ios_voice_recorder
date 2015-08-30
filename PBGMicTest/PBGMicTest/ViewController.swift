03b3cfb15383732214a71d9a6630f022cd84fe53
sakairyota <sakai@cloudstudy.com>
2015-08-30 12:22:46 +0900

    Initial commit

---
+++ PBGMicTest/PBGMicTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PBGMicTest
+//
+//  Created by 境 良太 on 2015/08/30.
+//  Copyright (c) 2015年 PBGreen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
