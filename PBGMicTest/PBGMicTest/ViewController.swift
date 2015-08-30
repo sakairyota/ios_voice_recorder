2774e27608ae8c85ff0beac99b2e403e0cdf0e86
sakairyota <sakai@cloudstudy.com>
2015-08-30 13:00:25 +0900

    ガワ作成

--- PBGMicTest/PBGMicTest/ViewController.swift
+++ PBGMicTest/PBGMicTest/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var isRecording: Bool = false
+    private var isPlaying: Bool = false
+    private var hasRecorded: Bool = false
+
+    @IBOutlet weak var statusLabelView: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        updateStatusText()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,49 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTouchDownRecord(sender: UIButton) {
+        isRecording = true
 
+        updateStatusText()
+    }
+
+    @IBAction func onTouchUpRecord(sender: UIButton) {
+        isRecording = false
+        hasRecorded = true
+
+        updateStatusText()
+    }
+
+    @IBAction func onTouchDownPlay(sender: UIButton) {
+        isPlaying = true
+
+        updateStatusText()
+    }
+
+    @IBAction func onTouchUpPlay(sender: UIButton) {
+        isPlaying = false
+
+        updateStatusText()
+    }
+
+    private func updateStatusText() {
+        let statusText: String
+        if isRecording {
+            if isPlaying {
+                statusText = "録音＆再生中"
+            } else {
+                statusText = "録音中"
+            }
+        } else {
+            if isPlaying {
+                statusText = "再生中"
+            } else if hasRecorded {
+                statusText = "..."
+            } else {
+                statusText = "録音してください"
+            }
+        }
+        statusLabelView.text = statusText
+    }
 }
 
